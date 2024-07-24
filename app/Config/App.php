080d1bf86d6c1c4c42192d9940fdfd0f60ab4b2b
Raihan Yudi Syukma <153145965+raihan-yudi-syukma@users.noreply.github.com>
2024-07-24 23:28:43 +0700

    Update App.php
    Mengganti value "$baseUrl" yang sebelumnya adalah "http://iaz-forum.test" (contoh virtual host) menjadi "localhost/iaz-forum".

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://iaz-forum.test';
+    public string $baseURL = 'localhost/iaz-forum/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
