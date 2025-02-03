77bef679ce448c91144681dc319bc562ffc2e46b
_RAINHARDER <raihanys03@gmail.com>
2025-02-03 20:03:42 +0700

    Just small update

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,9 +10,13 @@ $routes->setAutoRoute(true);
 
 $routes->set404Override('App\Controllers\Errors::error404');
 $routes->setDefaultNamespace('App\Controllers');
+
+// Set default controller and method.
 $routes->setDefaultController('Home');
 $routes->setDefaultMethod('index');
+$routes->get('/', 'Home::index');
 
+// Set routes for auth.
 $routes->get('/login', 'Auth::login');
 $routes->get('/register', 'Auth::register');
 $routes->get('/logout', 'Auth::logout');
